b5368fcd9593e672f03e9c016ffa2553ae10df3a
Peter Preeper <ppreeper@gmail.com>
2024-01-16 16:55:29 -0700

    update setup command

--- src/setup_command.sh
+++ src/setup_command.sh
@@ -1,10 +1,9 @@
-sudo apt update -y
-sudo apt install -y python3-pip python3-venv direnv git
-pip install pipenv
+sudo apt update -y && sudo apt install -y python3-pip python3-venv pipenv direnv git
+
 if grep -q "direnv hook bash" $HOME/.bashrc; then
   echo
 else
   echo 'eval "$(direnv hook bash)"' >> $HOME/.bashrc
 fi
-git clone https://github.com/ppreeper/opensetup $HOME/opensetup
+
 source $HOME/.bashrc
